1d0a48f4317076f73e1a0418032cfa32c0cebee5
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2024-07-10 18:04:27 +0300

    build: redis management of user data before persisntence to db

--- app/index.ts
+++ app/index.ts
@@ -14,6 +14,7 @@ import { graphql } from 'graphql';
 import { schema } from './schema';
 import { contextFactory } from './context';
 import Client from './redisClient';
+import redisClient from './redisClient';
 
 interface VerifyEmailQuery {
   token: string;
@@ -152,7 +153,8 @@ async function app() {
           if (valid) {
             // Return a JSON response with the redirect URL
             // resp.status(200).send({ redirectUrl });
-            reply.header('Set-Cookie', `token=${token}; Path=/; HttpOnly`);
+            // reply.header('Set-Cookie', `token=${token}; Path=/; HttpOnly`);
+            reply.header('Set-Cookie', `token=${token}; Path=/;`);
             reply.redirect(redirectUrl || '/');
           } else {
             reply.status(400).send(message || 'Verification failed');
@@ -167,6 +169,35 @@ async function app() {
     },
   });
 
+  //Retrieve information from redis
+  server.route({
+    method: 'POST',
+    url: '/api/redis/user-data',
+    handler: async (req, reply) => {
+      const token = req.headers.authorization?.split(' ')[1];
+      console.log(req.headers)
+      if (!token) {
+        reply.status(401).send('Token is required');
+        return;
+      }
+
+      try {
+        const userData = await redisClient.get(token);
+
+        if (userData) {
+          reply.send(JSON.parse(userData));
+        } else {
+          reply.status(404).send('User data not found');
+        }
+      }
+      catch (error) {
+        console.log('Error fetching user data from Redis:', error);
+        reply.status(500).send('Internal Server Error');
+      }
+    }
+  })
+
+  //Server listening
   server.listen({ port: port }, (err, address) => {
     if (err) {
       console.error(err);
