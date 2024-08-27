57beb7d5adb5eaaa754f4b2b9e95617945c69bf8
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2024-08-27 09:01:55 +0300

    build: small changes in authentication of current user

--- app/auth.ts
+++ app/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, User } from '@prisma/client';
 import { FastifyRequest } from 'fastify';
 import jwt from 'jsonwebtoken';
+import { JwtPayload, verify } from 'jsonwebtoken';
 
 export const APP_SECRET = 'secret';
 
@@ -12,17 +13,29 @@ async function authenticateUser(
   prisma: PrismaClient,
   request: FastifyRequest
 ): Promise<User | null> {
-  if (request.headers.authorization) {
-    const token = request.headers.authorization.split(' ')[1];
-    // const tokenPayload = verify(token, APP_SECRET) as JwtPayload;
-    const tokenPayload = jwt.verify(token, APP_SECRET) as CustomJwtPayload
-    const userId = tokenPayload.userId;
 
-    return await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
+  const authHeader = request.headers.authorization;
+
+  if (authHeader) {
+
+    try {
+      const token = authHeader.split(' ')[1];
+      const tokenPayload = verify(token, APP_SECRET) as CustomJwtPayload
+
+      if (tokenPayload && tokenPayload.userId) {
+
+        return await prisma.user.findUnique({
+          where: {
+            id: tokenPayload.userId,
+          }
+        });
+      }
+    }
+
+    catch (error) {
+      console.error("Error verifying token", error);
+      return null
+    }
   }
   return null;
 }
--- dist/auth.js
+++ dist/auth.js
@@ -1,22 +1,26 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.APP_SECRET = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const jsonwebtoken_1 = require("jsonwebtoken");
 exports.APP_SECRET = 'secret';
 async function authenticateUser(prisma, request) {
-    if (request.headers.authorization) {
-        const token = request.headers.authorization.split(' ')[1];
-        // const tokenPayload = verify(token, APP_SECRET) as JwtPayload;
-        const tokenPayload = jsonwebtoken_1.default.verify(token, exports.APP_SECRET);
-        const userId = tokenPayload.userId;
-        return await prisma.user.findUnique({
-            where: {
-                id: userId,
-            },
-        });
+    const authHeader = request.headers.authorization;
+    if (authHeader) {
+        try {
+            const token = authHeader.split(' ')[1];
+            const tokenPayload = (0, jsonwebtoken_1.verify)(token, exports.APP_SECRET);
+            if (tokenPayload && tokenPayload.userId) {
+                return await prisma.user.findUnique({
+                    where: {
+                        id: tokenPayload.userId,
+                    }
+                });
+            }
+        }
+        catch (error) {
+            console.error("Error verifying token", error);
+            return null;
+        }
     }
     return null;
 }
