b75ac113c2c72a847c527532178729a2dfd73848
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2024-11-21 01:39:18 +0300

    build: tests pass

--- dist/src/schema.js
+++ dist/src/schema.js
@@ -14,7 +14,7 @@ const typeDefs = (0, graphql_tag_1.default)(fs_1.default.readFileSync(path_1.def
 exports.resolvers = {
     Query: Object.assign(Object.assign({ info: (parent, args, context) => {
             return `This is the parent: ${parent}. These are the ${args}. This is the ${context}. This works!`;
-        } }, userController_1.userResolvers.Query), orderController_1.orderResolvers.Query),
+        }, test: (parent, args, context) => `This is the parent: ${parent}. These are the ${args}. This is the ${context}. This works!` }, userController_1.userResolvers.Query), orderController_1.orderResolvers.Query),
     Mutation: Object.assign(Object.assign({}, userController_1.userResolvers.Mutation), orderController_1.orderResolvers.Mutation),
 };
 exports.schema = (0, schema_1.makeExecutableSchema)({
